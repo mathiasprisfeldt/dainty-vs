0880371086f6bff17652c9d42008f0c02cdc7ff5
Alexander Teinum <ateinum@gmail.com>
2018-11-24 14:05:44 +0100

    Update remaining adjustment function

--- src/replacements/indent-guides.js
+++ src/replacements/indent-guides.js
@@ -5,13 +5,13 @@ function getIndentGuidesReplacements(configuration, colors) {
   const { blueGray, purple } = colors;
   const dark = configuration.variant === "dark";
 
-  function edbc(index) {
-    return checkScaleRange(index - editor.backgroundLightness);
+  function edbl(index) {
+    return checkScaleRange(index + editor.backgroundLightness);
   }
 
   return [
-    ["DEFAULT_DEFAULT_LINE_COLOR", toRGBString(blueGray[edbc(4)])],
-    ["DEFAULT_DEFAULT_HIGHLIGHT_COLOR", toRGBString(blueGray[edbc(6)])],
+    ["DEFAULT_DEFAULT_LINE_COLOR", toRGBString(blueGray[edbl(4)])],
+    ["DEFAULT_DEFAULT_HIGHLIGHT_COLOR", toRGBString(blueGray[edbl(6)])],
     ["DEFAULT_UNALIGNED_LINE_COLOR", toRGBString(purple[39])],
     ["DEFAULT_UNALIGNED_HIGHLIGHT_COLOR", toRGBString(purple[39])]
   ];
